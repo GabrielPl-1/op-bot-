793dcc97708c4e43ce7662d50d5ed7872be0793a
GabrielPl-1 <62753064+GabrielPl-1@users.noreply.github.com>
2020-10-06 17:09:59 -0300

    Create Codigo_pronto.py

---
+++ Codigo_pronto.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+client=discord.Client()
+arquivo=open('listausuarios.txt','x')
+arquivo2=open('listanicks.txt','x')
+
+@client.event
+async def on_ready():
+	print('Loggd in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+	print('bot is ready')
+	
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+		
+	if message.content.startswith("!op"):
+		message.content = message.content[4:]
+		arquivo.write(message.content)
+		arquivo.write('\n')
+		arquivo2.write(message.author.name)
+		arquivo2.write('\n')
+		message.content = message.content.replace(' ','+')
+		response=f'https://br.op.gg/summoner/userName={message.content}'
+		await message.channel.send(response)
+		
+		
+client.run('NzU0NzM4MzQzNTkzMzc3ODYy.X15G2w.VSy5oftwlXmUuCNUpXbQgYALrCg')
+arquivo.close()
+arquivo2.close()
